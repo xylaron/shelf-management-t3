2471f95c9b319b262bcec3f286fc61ee6930f1df
xylaron <aldo6701@gmail.com>
2022-12-23 02:56:27 +0800

    clean up boilerplate code

---
+++ src/components/example/ExampleQuery.tsx
@@ -0,0 +1,16 @@
+import { trpc } from "../../utils/trpc";
+
+const ExampleQuery: React.FC = () => {
+  const hello: any = trpc.example.hello.useQuery({ text: "from tRPC" });
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <p className="text-2xl text-white">
+        {/* Message obtained from tRPC */}
+        Example Message from tRPC query:{" "}
+        {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+      </p>
+    </div>
+  );
+};
+
+export default ExampleQuery;
