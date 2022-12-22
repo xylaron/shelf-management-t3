639c25419c5dae6f9aa43ca3293ea639cb186168
xylaron <aldo6701@gmail.com>
2022-12-23 02:02:35 +0700

    test

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,21 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Products: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Products - Shelf Management App</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1 className="tracking text-5xl font-extrabold text-white sm:text-[5rem]">
+            <span className="text-purple-500">Products</span> Page
+          </h1>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Products;
