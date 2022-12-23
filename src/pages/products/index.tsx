a794626978f63ff521ec9ed6f51bc442199c0507
xylaron <aldo6701@gmail.com>
2022-12-24 00:48:56 +0700

    display data from products table

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,7 +1,11 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import { trpc } from "../../utils/trpc";
 
 const Products: NextPage = () => {
+  const products = trpc.products.getAll.useQuery();
+  const productList = products.data || [];
+
   return (
     <>
       <Head>
@@ -12,6 +16,30 @@ const Products: NextPage = () => {
           <h1 className="tracking text-5xl font-extrabold text-white sm:text-[5rem]">
             <span className="text-purple-500">Products</span> Page
           </h1>
+          <div className="flex flex-col items-center justify-center gap-12">
+            {productList.length == 0 ? (
+              <div>
+                <div className="text-2xl text-white">No products found</div>
+              </div>
+            ) : (
+              productList.map((product) => (
+                <div
+                  key={product.id}
+                  className="flex flex-col items-center justify-center  "
+                >
+                  <p className="text-2xl text-white">
+                    Product Name: {product.name}
+                  </p>
+                  <p className="text-2xl text-white">
+                    Product Price: ${product.price}
+                  </p>
+                  <p className="text-2xl text-white">
+                    Product Amount: {product.amount}
+                  </p>
+                </div>
+              ))
+            )}
+          </div>
         </div>
       </main>
     </>
