0ee64033b4902faaab9dd21ad992f5d33c053e8c
xylaron <aldo6701@gmail.com>
2022-12-25 00:12:28 +0700

    fix page/component styles

--- src/components/example/ExampleQuery.tsx
+++ src/components/example/ExampleQuery.tsx
@@ -4,7 +4,7 @@ const ExampleQuery: React.FC = () => {
   const hello: any = trpc.example.hello.useQuery({ text: "from tRPC" });
   return (
     <div className="flex flex-col items-center gap-2">
-      <p className="text-2xl text-white">
+      <p className="text-lg text-white">
         {/* Message obtained from tRPC */}
         Example Message from tRPC query:{" "}
         {hello.data ? hello.data.greeting : "Loading tRPC query..."}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="tracking text-5xl font-extrabold text-white sm:text-[5rem]">
+          <h1 className="tracking text-6xl font-extrabold text-white">
             <span className="text-purple-500">Home</span> Page
           </h1>
           <ExampleQuery />
