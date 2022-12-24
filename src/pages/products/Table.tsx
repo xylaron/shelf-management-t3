f3ae627f55e2eacef2b42bdc7f9657193fedf6a4
xylaron <aldo6701@gmail.com>
2022-12-25 00:12:06 +0700

    change product page to display table from data

---
+++ src/pages/products/Table.tsx
@@ -0,0 +1,35 @@
+import { trpc } from "../../utils/trpc";
+
+const ProductTable = () => {
+  const products = trpc.products.getAll.useQuery();
+  const productList = products.data || [];
+
+  if (productList.length === 0) {
+    return <div className="text-xl text-white">No Products Found</div>;
+  }
+
+  const table = productList.map((product) => {
+    return (
+      <tr>
+        <td>{product.id}</td>
+        <td>{product.name}</td>
+        <td>${product.price}</td>
+        <td>{product.amount}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Amount</th>
+      </thead>
+      <tbody>{table}</tbody>
+    </table>
+  );
+};
+
+export default ProductTable;
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,44 +1,20 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import { trpc } from "../../utils/trpc";
+import Table from "./Table";
 
 const Products: NextPage = () => {
-  const products = trpc.products.getAll.useQuery();
-  const productList = products.data || [];
-
   return (
     <>
       <Head>
         <title>Products - Shelf Management App</title>
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="tracking text-5xl font-extrabold text-white sm:text-[5rem]">
+      <main className="flex min-h-screen flex-col items-center">
+        <div className="container flex flex-col items-center gap-12 px-4 py-16 ">
+          <h1 className="text-6xl font-extrabold text-white">
             <span className="text-purple-500">Products</span> Page
           </h1>
-          <div className="flex flex-col items-center justify-center gap-12">
-            {productList.length == 0 ? (
-              <div>
-                <div className="text-2xl text-white">No products found</div>
-              </div>
-            ) : (
-              productList.map((product) => (
-                <div
-                  key={product.id}
-                  className="flex flex-col items-center justify-center  "
-                >
-                  <p className="text-2xl text-white">
-                    Product Name: {product.name}
-                  </p>
-                  <p className="text-2xl text-white">
-                    Product Price: ${product.price}
-                  </p>
-                  <p className="text-2xl text-white">
-                    Product Amount: {product.amount}
-                  </p>
-                </div>
-              ))
-            )}
+          <div className="flex flex-col items-center justify-center gap-12 ">
+            <Table />
           </div>
         </div>
       </main>
