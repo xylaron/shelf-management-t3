2d31b46181366df1c4a5fe2044502d5d341193fc
xylaron <aldo6701@gmail.com>
2022-12-29 01:24:31 +0700

    config data generators

--- src/utils/generateData.ts
+++ src/utils/generateData.ts
@@ -1,10 +1,11 @@
 import { faker } from "@faker-js/faker";
 import { Transaction } from "types";
+import { sampleProducts } from "utils/sampleProducts";
 
-export const generateNames = () => {
+export const generateNames = (dataNum: number) => {
   const names = new Array<string>();
   const checkNames = new Set<string>();
-  while (names.length < 100) {
+  while (names.length < dataNum) {
     const name = faker.name.firstName() + " " + faker.name.lastName();
     if (!checkNames.has(name)) {
       names.push(name);
@@ -14,15 +15,17 @@ export const generateNames = () => {
   return names;
 };
 
-export const generateTransactions = () => {
+export const generateTransactions = (dataNum: number) => {
   const transactions = new Array<Transaction>();
   const openingTime = new Date(2020, 0, 1, 9, 0, 0); // 9:00 AM
   const closingTime = new Date(2020, 0, 1, 22, 0, 0); // 10:00 PM
-  while (transactions.length < 10) {
+  while (transactions.length < dataNum) {
     const transaction: Transaction = {} as Transaction;
-    transaction.id = transactions.length;
-    transaction.product = faker.commerce.product();
-    transaction.quantity = Math.round(faker.datatype.float({ max: 10 }));
+    transaction.id = transactions.length + 1;
+    transaction.product =
+      sampleProducts[faker.datatype.number({ max: sampleProducts.length - 1 })]
+        ?.name || "";
+    transaction.quantity = faker.datatype.number({ min: 1, max: 10 });
     transaction.time = faker.date.between(openingTime, closingTime);
     transactions.push(transaction);
   }
