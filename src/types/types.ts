53ecd0e2625f7b70bbab8f0954f76f41c6fc0869
xylaron <aldo6701@gmail.com>
2022-12-31 02:18:34 +0700

    removed local type product

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,13 +1,6 @@
-export interface Transaction {
+export interface Transactions {
   id: number;
   product: string;
   quantity: number;
   time: Date;
-}
-
-export interface Product {
-  name: string;
-  type: string;
-  size: number;
-  price: number;
 }
