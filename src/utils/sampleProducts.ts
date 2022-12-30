4aff62b6b10f40cb097722f2bda27fdf33df7b66
xylaron <aldo6701@gmail.com>
2022-12-31 02:23:11 +0700

    change sample product quantity and interface name

--- src/utils/sampleProducts.ts
+++ src/utils/sampleProducts.ts
@@ -1,8 +1,11 @@
-import { Product } from "types";
-
-// prettier-ignore
-
-export const sampleProducts: Product[] = [
+interface SampleProduct {
+  name: string;
+  type: string;
+  size: number;
+  price: number;
+}
+//prettier-ignore
+export const sampleProducts: SampleProduct[] = [
   { name: 'Aquafina', type: 'Bottled Water', size: 500, price: 1.50},
   { name: 'Coca-Cola', type: 'Soft Drink', size: 355, price: 1.75},
   { name: 'Red Bull', type: 'Energy Drink', size: 250, price: 2.50},
@@ -13,14 +16,4 @@ export const sampleProducts: Product[] = [
   { name: 'Starbucks Coffee', type: 'Coffee', size: 355, price: 2.50},
   { name: 'Lipton Tea', type: 'Tea', size: 200, price: 3.00},
   { name: 'Budweiser', type: 'Beer', size: 355, price: 1.50},
-  { name: 'Chardonnay', type: 'Wine', size: 750, price: 15.00},
-  { name: 'Jack Daniel\'s', type: 'Whiskey', size: 750, price: 30.00},
-  { name: 'Absolut Vodka', type: 'Vodka', size: 750, price: 20.00},
-  { name: 'Bailey\'s Irish Cream', type: 'Liqueur', size: 750, price: 25.00},
-  { name: 'Bombay Sapphire', type: 'Gin', size: 750, price: 30.00},
-  { name: 'Captain Morgan\'s', type: 'Rum', size: 750, price: 20.00},
-  { name: 'Jägermeister', type: 'Liqueur', size: 750, price: 25.00},
-  { name: 'Kahlua', type: 'Liqueur', size: 750, price: 25.00},
-  { name: 'Malibu', type: 'Rum', size: 750, price: 20.00},
-  { name: 'Tequila', type: 'Tequila', size: 750, price: 30.00}
 ];
