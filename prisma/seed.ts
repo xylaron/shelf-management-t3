ae9970fa519158d5b0153c7436711805dee6423d
xylaron <aldo6701@gmail.com>
2023-01-18 15:04:19 +0800

    change reset query and add transaction generator

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,31 @@
+import { generateTransactions } from "utils/generateData";
 import { prisma } from "../src/server/db/client";
 import { sampleProducts } from "utils/sampleProducts";
 
 async function main() {
   if (process.env.NODE_ENV !== "development") return;
 
-  // Delete all existing data
-  await prisma.products.deleteMany();
-  await prisma.transactions.deleteMany();
-  // Reset the autoincrement counter
-  await prisma.$queryRaw`UPDATE sqlite_sequence SET seq = 0 WHERE name = 'Products'`;
+  // Delete Reset the autoincrement counter using raw (differs from db solutions)
+  // This method is for Postgresql ONLY
+  await prisma.$queryRaw`TRUNCATE Products RESTART IDENTITY;`;
+  await prisma.$queryRaw`TRUNCATE Transactions RESTART IDENTITY`;
 
+  //Assigns data from sampleProducts
   for (const product of sampleProducts) {
     const item = await prisma.products.create({
       data: product,
+    });
+    console.log(item);
+  }
+
+  //Assigns data from generateTransactions funcion
+  for (const transaction of generateTransactions(1000)) {
+    const item = await prisma.transactions.create({
+      data: {
+        productId: transaction.productId,
+        quantity: transaction.quantity,
+        time: transaction.time,
+      },
     });
     console.log(item);
   }
