cb5a2df7245234f8b3bb79aeeaab70fe359b2b53
xylaron <aldo6701@gmail.com>
2023-01-18 15:07:03 +0800

    update product list

--- src/utils/sampleProducts.ts
+++ src/utils/sampleProducts.ts
@@ -1,4 +1,4 @@
-interface SampleProduct {
+export interface SampleProduct {
   name: string;
   type: string;
   size: number;
@@ -6,14 +6,14 @@ interface SampleProduct {
 }
 //prettier-ignore
 export const sampleProducts: SampleProduct[] = [
-  { name: 'Aquafina', type: 'Bottled Water', size: 500, price: 1.50},
-  { name: 'Coca-Cola', type: 'Soft Drink', size: 355, price: 1.75},
-  { name: 'Red Bull', type: 'Energy Drink', size: 250, price: 2.50},
-  { name: 'Tropicana Orange Juice', type: 'Fruit Juice', size: 355, price: 2.25},
-  { name: 'Lipton Iced Tea', type: 'Iced Tea', size: 500, price: 1.50},
-  { name: 'Gatorade', type: 'Sports Drink', size: 591, price: 2.00},
-  { name: 'Milk', type: 'Milk', size: 3785, price: 3.50},
-  { name: 'Starbucks Coffee', type: 'Coffee', size: 355, price: 2.50},
-  { name: 'Lipton Tea', type: 'Tea', size: 200, price: 3.00},
-  { name: 'Budweiser', type: 'Beer', size: 355, price: 1.50},
+  { name: 'Bonaqua', type: 'Bottled Water', size: 500, price: 8.00},
+  { name: 'Coca-Cola', type: 'Soft Drink', size: 350, price: 7.00},
+  { name: 'Red Bull', type: 'Energy Drink', size: 500, price: 15.00},
+  { name: 'Minute Maid Orange Juice', type: 'Fruit Juice', size: 420, price: 7.00},
+  { name: 'Lipton Iced Tea', type: 'Iced Tea', size: 400, price: 6.50},
+  { name: 'Lucozade', type: 'Sports Drink', size: 600, price: 15.00},
+  { name: 'Sprite', type: 'Soft Drink', size: 350, price: 7.00},
+  { name: 'Starbucks Coffee', type: 'Coffee', size: 350, price: 25.00},
+  { name: 'Lipton Tea', type: 'Tea', size: 200, price: 6.00},
+  { name: 'Budweiser', type: 'Beer', size: 350, price: 9.50},
 ];
