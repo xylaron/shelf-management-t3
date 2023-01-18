42f3be89fe0d7fffc5c67f92feae1f520cc5b8ae
xylaron <aldo6701@gmail.com>
2023-01-18 15:08:45 +0800

    update transactions generator

--- src/utils/generateData.ts
+++ src/utils/generateData.ts
@@ -1,10 +1,13 @@
 import { faker } from "@faker-js/faker";
-import { type Transactions } from "types";
+import { type Transactions } from "@prisma/client";
 import { sampleProducts } from "utils/sampleProducts";
 
 export const generateNames = (dataNum: number) => {
+  //initialize variables/constraints/array
   const names = new Array<string>();
   const checkNames = new Set<string>();
+
+  //generate data
   while (names.length < dataNum) {
     const name = faker.name.firstName() + " " + faker.name.lastName();
     if (!checkNames.has(name)) {
@@ -16,18 +19,42 @@ export const generateNames = (dataNum: number) => {
 };
 
 export const generateTransactions = (dataNum: number) => {
+  //initialize variables/constraints/array
   const transactions = new Array<Transactions>();
-  const openingTime = new Date(2020, 0, 1, 9, 0, 0); // 9:00 AM
-  const closingTime = new Date(2020, 0, 1, 22, 0, 0); // 10:00 PM
+
+  const startingHour = new Date().setHours(9, 0, 0, 0);
+  const closingHour = new Date().setHours(23, 0, 0, 0);
+
+  const startDate = new Date(2022, 0, 1);
+  const endDate = new Date(2022, 0, 31);
+
+  //generate data
   while (transactions.length < dataNum) {
     const transaction: Transactions = {} as Transactions;
     transaction.id = transactions.length + 1;
-    transaction.product =
-      sampleProducts[faker.datatype.number({ max: sampleProducts.length - 1 })]
-        ?.name || "";
+    transaction.productId = faker.datatype.number({
+      min: 1,
+      max: sampleProducts.length,
+    });
     transaction.quantity = faker.datatype.number({ min: 1, max: 10 });
-    transaction.time = faker.date.between(openingTime, closingTime);
+
+    const hour = faker.date.between(startingHour, closingHour);
+    const date = faker.date.between(startDate, endDate);
+    transaction.time = new Date(date.setHours(hour.getHours()));
+
     transactions.push(transaction);
   }
+
+  //sort data by time
+  transactions.sort((a, b) => {
+    return a.time.getTime() - b.time.getTime();
+  });
+
+  //reassign id
+  transactions.forEach((transaction, index) => {
+    transaction.id = index + 1;
+  });
+
+  console.log(transactions);
   return transactions;
 };
