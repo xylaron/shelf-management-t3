95a017012e0b2011b49a118f2a9a9749e6e5cc90
xylaron <aldo6701@gmail.com>
2023-01-18 15:05:42 +0800

    update table data

--- src/components/transactions/Table.tsx
+++ src/components/transactions/Table.tsx
@@ -1,15 +1,17 @@
-import { type Transactions } from "types";
+import { type Transactions } from "@prisma/client";
+import { sampleProducts } from "utils/sampleProducts";
+
 //prettier-ignore
-const Table: React.FC<{ transactionsList: Transactions[] }> = ({ transactionsList}) => {
+const Table: React.FC<{ transactionsList: Transactions[] }> = ({ transactionsList }) => {
   if (transactionsList.length === 0) {
     return <div className="text-xl text-white">No Transactions Found</div>;
   }
 
-  const table = transactionsList.map(({ id, product, quantity, time }) => {
+  const table = transactionsList.map(({ id, productId, quantity, time }) => {
     return (
       <tr key={id}>
         <td>{id}</td>
-        <td>{product}</td>
+        <td>{sampleProducts[productId - 1]?.name}</td>
         <td>{quantity}</td>
         <td>{time.toString()}</td>
       </tr>
@@ -19,10 +21,12 @@ const Table: React.FC<{ transactionsList: Transactions[] }> = ({ transactionsLis
   return (
     <table>
       <thead>
-        <th>ID</th>
-        <th>Product</th>
-        <th>Quantity</th>
-        <th>Time</th>
+        <tr>
+          <th>ID</th>
+          <th>Product</th>
+          <th>Quantity</th>
+          <th>Time</th>
+        </tr>
       </thead>
       <tbody>{table}</tbody>
     </table>
