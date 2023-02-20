bc214f282637b1225fb947e78ae4f3fe552f5d8e
xylaron <aldo6701@gmail.com>
2023-02-21 05:46:52 +0800

    update generator functions to match db schema

--- src/utils/generateData.ts
+++ src/utils/generateData.ts
@@ -1,5 +1,8 @@
 import { faker } from "@faker-js/faker";
-import { type Transactions } from "@prisma/client";
+import {
+  type Transactions,
+  Transactions_Products as SingleTransaction,
+} from "@prisma/client";
 import { sampleProducts } from "utils/sampleProducts";
 
 //to be removed later
@@ -19,43 +22,107 @@ export const generateNames = (dataNum: number) => {
   return names;
 };
 
-export const generateTransactions_OLD = (dataNum: number) => {
-  //initialize variables/constraints/array
+export const generateSingleTransaction = (totalTransactions: number) => {
+  const singleTransactions = new Array<SingleTransaction>();
+
+  for (let i = 0; i < totalTransactions; i++) {
+    const boughtProductsWeightPool = [1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3];
+
+    const amountOfBoughtProducts =
+      boughtProductsWeightPool[
+        faker.datatype.number({
+          min: 1,
+          max: boughtProductsWeightPool.length - 1,
+        })
+      ] || 1;
+
+    for (let j = 0; j < amountOfBoughtProducts; j++) {
+      const singleTransaction = {} as SingleTransaction;
+
+      singleTransaction.transactionId = i + 1;
+      singleTransaction.productId = faker.datatype.number({
+        min: 1,
+        max: sampleProducts.length - 1,
+      });
+      const productAmountWeightPool = [
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5,
+      ];
+      singleTransaction.quantity =
+        productAmountWeightPool[
+          faker.datatype.number({ min: 1, max: productAmountWeightPool.length })
+        ] || 1;
+      singleTransaction.total_price =
+        singleTransaction.quantity *
+        // @ts-ignore - to be fixed
+        sampleProducts[singleTransaction.productId - 1].price;
+
+      singleTransactions.push(singleTransaction);
+    }
+  }
+
+  const checkDuplicates = new Set<string>();
+  const filteredSingleTransactions = singleTransactions.filter((item) => {
+    const key = `${item.transactionId}-${item.productId}`;
+    const duplicate = checkDuplicates.has(key);
+    checkDuplicates.add(key);
+    return !duplicate;
+  });
+
+  return {
+    totalTransactions: totalTransactions,
+    singleTransactions: filteredSingleTransactions,
+  };
+};
+
+export const generateTransactions = (singleTransactionsOutput: {
+  totalTransactions: number;
+  singleTransactions: SingleTransaction[];
+}) => {
+  const { totalTransactions, singleTransactions } = singleTransactionsOutput;
+
   const transactions = new Array<Transactions>();
 
-  const startingHour = new Date().setHours(9, 0, 0, 0);
-  const closingHour = new Date().setHours(23, 0, 0, 0);
+  const startingHour = new Date(0, 0, 0, 9, 0, 0, 0);
+  const closingHour = new Date(0, 0, 0, 23, 0, 0, 0);
 
-  const startDate = new Date(2022, 0, 1);
-  const endDate = new Date(2022, 0, 31);
+  const startDate = new Date(2023, 0, 1);
+  const endDate = new Date(2023, 0, 2);
 
-  //generate data
-  while (transactions.length < dataNum) {
-    const transaction: Transactions = {} as Transactions;
-    transaction.id = transactions.length + 1;
-    transaction.productId = faker.datatype.number({
-      min: 1,
-      max: sampleProducts.length,
-    });
-    transaction.quantity = faker.datatype.number({ min: 1, max: 10 });
+  for (let i = 0; i < totalTransactions; i++) {
+    const transaction = {} as Transactions;
+
+    transaction.id = i + 1;
 
     const hour = faker.date.between(startingHour, closingHour);
     const date = faker.date.between(startDate, endDate);
+
     transaction.time = new Date(date.setHours(hour.getHours()));
 
     transactions.push(transaction);
   }
 
-  //sort data by time
   transactions.sort((a, b) => {
     return a.time.getTime() - b.time.getTime();
   });
 
-  //reassign id
   transactions.forEach((transaction, index) => {
     transaction.id = index + 1;
   });
 
-  console.log(transactions);
+  for (let i = 0; i < totalTransactions; i++) {
+    const filteredSingleTransactions = singleTransactions.filter(
+      (singleTransaction) => singleTransaction.transactionId === i + 1
+    );
+
+    const transaction = transactions[i] as Transactions;
+
+    transaction.total_price = filteredSingleTransactions.reduce(
+      (acc, filteredSingleTransactions) => {
+        return acc + filteredSingleTransactions.total_price;
+      },
+      0
+    );
+  }
+
   return transactions;
 };
