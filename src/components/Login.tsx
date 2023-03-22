ccdbe872bdd7d81fe51ad4da3678a4390b166627
xylaron <aldo6701@gmail.com>
2023-03-22 21:37:57 +0800

    added login page header

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { trpc } from "utils/trpc";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 const Login: React.FC<{
   setIsAuthenticated: Dispatch<SetStateAction<boolean>>;
@@ -28,41 +29,46 @@ const Login: React.FC<{
   };
 
   return (
-    <main className="flex flex-col items-center justify-center">
-      {user.isLoading ? (
-        <div className="text-xl">Loading...</div>
-      ) : user.isError ? (
-        <div className="text-xl">Error: {user.error.message}</div>
-      ) : (
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-6xl font-extrabold text-white">
-            <span>Login</span> Page
-          </h1>
-          <div>
-            <form onSubmit={handleSubmit}>
-              <div className="flex flex-row gap-4">
-                <input
-                  className="rounded bg-neutral-700 px-4 py-2"
-                  id="password"
-                  type="password"
-                />
-                <button
-                  className="rounded bg-purple-700 py-2 px-4 font-bold transition-colors hover:bg-purple-800 active:bg-purple-900"
-                  type="submit"
-                >
-                  Login
-                </button>
+    <>
+      <Head>
+        <title>Login - Shelf Management App</title>
+      </Head>
+      <main className="flex flex-col items-center justify-center">
+        {user.isLoading ? (
+          <div className="text-xl">Loading...</div>
+        ) : user.isError ? (
+          <div className="text-xl">Error: {user.error.message}</div>
+        ) : (
+          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+            <h1 className="text-6xl font-extrabold text-white">
+              <span>Login</span> Page
+            </h1>
+            <div>
+              <form onSubmit={handleSubmit}>
+                <div className="flex flex-row gap-4">
+                  <input
+                    className="rounded bg-neutral-700 px-4 py-2"
+                    id="password"
+                    type="password"
+                  />
+                  <button
+                    className="rounded bg-purple-700 py-2 px-4 font-bold transition-colors hover:bg-purple-800 active:bg-purple-900"
+                    type="submit"
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+              <div className="mt-4 text-center font-bold">
+                {passwordAlert && (
+                  <div className="text-red-500">Incorrect password</div>
+                )}
               </div>
-            </form>
-            <div className="mt-4 text-center font-bold">
-              {passwordAlert && (
-                <div className="text-red-500">Incorrect password</div>
-              )}
             </div>
           </div>
-        </div>
-      )}
-    </main>
+        )}
+      </main>
+    </>
   );
 };
 
