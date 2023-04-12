540d39a2a2b3560c792d38620d96f9382819206b
xylaron <aldo6701@gmail.com>
2023-04-13 05:13:56 +0800

    update imports

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { trpc } from "utils/trpc";
 import { useRouter } from "next/router";
 import Head from "next/head";
