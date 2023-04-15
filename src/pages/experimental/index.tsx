f8982c9c054917cc77cc6171700857c7c620d47d
xylaron <aldo6701@gmail.com>
2023-04-16 07:22:31 +0800

    added setup for layout generation

--- src/pages/experimental/index.tsx
+++ src/pages/experimental/index.tsx
@@ -1,10 +1,77 @@
-import { faker } from "@faker-js/faker";
+import { Products, Shelves, Transactions_Products } from "@prisma/client";
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { trpc } from "utils/trpc";
+
+interface ProductCount {
+  id: number;
+  count: number;
+}
+
+interface shelfLayout {
+  shelfId: number;
+  cubby: {
+    items: {
+      productId: number;
+      depthCount: number;
+    }[];
+  }[];
+}
 
 const Experimental: NextPage = () => {
-  const [number, setNumber] = useState<number>(727);
+  const [transactionsProductsList, setTransactionsProductsList] = useState<
+    Transactions_Products[]
+  >([]);
+  const [productList, setProductList] = useState<Products[]>([]);
+  const [productCount, setProductCount] = useState<ProductCount[]>([]);
+  const [shelf, setShelf] = useState<Shelves>();
+
+  const products = trpc.products.getAll.useQuery(undefined, {
+    onSuccess: (data) => {
+      setProductList(data ?? []);
+    },
+  });
+
+  const shelfByID = trpc.shelves.getById.useQuery(
+    { id: 1 },
+    {
+      onSuccess: (data) => {
+        setShelf(data!);
+      },
+    }
+  );
+
+  const transactionsProducts = trpc.transactionsProducts.getAll.useQuery(
+    undefined,
+    {
+      onSuccess: (data) => {
+        setTransactionsProductsList(data ?? []);
+      },
+    }
+  );
+
+  useEffect(() => {
+    console.log("Product Id Count:", getProductCount(transactionsProductsList));
+    setProductCount(getProductCount(transactionsProductsList));
+  }, [transactionsProductsList]);
+
+  const getProductCount = (
+    transactionsProductsList: Transactions_Products[]
+  ) => {
+    const productIds = transactionsProductsList.map(
+      (product) => product.productId
+    );
+    const productIdCountRecord = productIds.reduce((acc, curr) => {
+      acc[curr] = (acc[curr] || 0) + 1;
+      return acc;
+    }, {} as Record<number, number>);
+
+    return Object.entries(productIdCountRecord).map(([key, value]) => ({
+      id: parseInt(key),
+      count: value,
+    }));
+  };
 
   return (
     <>
@@ -13,18 +80,20 @@ const Experimental: NextPage = () => {
       </Head>
       <main className="flex flex-col items-center">
         <div className="container flex flex-col items-center gap-12 px-4 py-16">
-          <h1 className="tracking text-6xl font-extrabold">
-            <span>Experimental</span> Page
+          <h1 className="text-6xl font-extrabold">
+            <span>Generate</span> Layout
           </h1>
         </div>
-        <div className="flex flex-col gap-4 pt-8 pb-16">
-          <button
-            className="rounded-lg border border-white bg-purple-900 p-2 font-bold hover:bg-purple-900/80 active:scale-95 active:bg-purple-900/60"
-            onClick={() => setNumber(faker.datatype.number(1000))}
-          >
-            Generate Number
-          </button>
-          <div>{number === 727 ? "WYSI" : number}</div>
+        <div className="flex flex-col items-center justify-center gap-12 py-2">
+          {transactionsProducts.isLoading || products.isLoading ? (
+            <div className="text-xl">Loading...</div>
+          ) : transactionsProducts.isError ? (
+            <div className="text-xl">
+              Error: {transactionsProducts.error.message}
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
       </main>
     </>
